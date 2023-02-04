3c6aa3f69df3957b837a8a4361e2d366807e8a2b
anafful <117636756+anafful@users.noreply.github.com>
2023-02-04 19:24:41 +0000

    Main Level Design

---
+++ Copy/Intermediate/Build/Win64/UnrealEditor/Inc/RootningAway/UHT/RootningAway.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeRootningAway_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_RootningAway;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_RootningAway()
+	{
+		if (!Z_Registration_Info_UPackage__Script_RootningAway.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/RootningAway",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x5620C085,
+				0x5C7D0789,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_RootningAway.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_RootningAway.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_RootningAway(Z_Construct_UPackage__Script_RootningAway, TEXT("/Script/RootningAway"), Z_Registration_Info_UPackage__Script_RootningAway, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x5620C085, 0x5C7D0789));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Copy/Intermediate/Build/Win64/UnrealEditor/Inc/RootningAway/UHT/RootningAwayGameMode.generated.h
@@ -0,0 +1,96 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "RootningAwayGameMode.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ROOTNINGAWAY_RootningAwayGameMode_generated_h
+#error "RootningAwayGameMode.generated.h already included, missing '#pragma once' in RootningAwayGameMode.h"
+#endif
+#define ROOTNINGAWAY_RootningAwayGameMode_generated_h
+
+#define FID_Githubprojects_GGJ2023_RootningAway_Source_RootningAway_RootningAwayGameMode_h_12_SPARSE_DATA
+#define FID_Githubprojects_GGJ2023_RootningAway_Source_RootningAway_RootningAwayGameMode_h_12_RPC_WRAPPERS
+#define FID_Githubprojects_GGJ2023_RootningAway_Source_RootningAway_RootningAwayGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_Githubprojects_GGJ2023_RootningAway_Source_RootningAway_RootningAwayGameMode_h_12_ACCESSORS
+#define FID_Githubprojects_GGJ2023_RootningAway_Source_RootningAway_RootningAwayGameMode_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesARootningAwayGameMode(); \
+	friend struct Z_Construct_UClass_ARootningAwayGameMode_Statics; \
+public: \
+	DECLARE_CLASS(ARootningAwayGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/RootningAway"), ROOTNINGAWAY_API) \
+	DECLARE_SERIALIZER(ARootningAwayGameMode)
+
+
+#define FID_Githubprojects_GGJ2023_RootningAway_Source_RootningAway_RootningAwayGameMode_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesARootningAwayGameMode(); \
+	friend struct Z_Construct_UClass_ARootningAwayGameMode_Statics; \
+public: \
+	DECLARE_CLASS(ARootningAwayGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/RootningAway"), ROOTNINGAWAY_API) \
+	DECLARE_SERIALIZER(ARootningAwayGameMode)
+
+
+#define FID_Githubprojects_GGJ2023_RootningAway_Source_RootningAway_RootningAwayGameMode_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	ROOTNINGAWAY_API ARootningAwayGameMode(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ARootningAwayGameMode) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(ROOTNINGAWAY_API, ARootningAwayGameMode); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ARootningAwayGameMode); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	ROOTNINGAWAY_API ARootningAwayGameMode(ARootningAwayGameMode&&); \
+	ROOTNINGAWAY_API ARootningAwayGameMode(const ARootningAwayGameMode&); \
+public: \
+	ROOTNINGAWAY_API virtual ~ARootningAwayGameMode();
+
+
+#define FID_Githubprojects_GGJ2023_RootningAway_Source_RootningAway_RootningAwayGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	ROOTNINGAWAY_API ARootningAwayGameMode(ARootningAwayGameMode&&); \
+	ROOTNINGAWAY_API ARootningAwayGameMode(const ARootningAwayGameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(ROOTNINGAWAY_API, ARootningAwayGameMode); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ARootningAwayGameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ARootningAwayGameMode) \
+	ROOTNINGAWAY_API virtual ~ARootningAwayGameMode();
+
+
+#define FID_Githubprojects_GGJ2023_RootningAway_Source_RootningAway_RootningAwayGameMode_h_9_PROLOG
+#define FID_Githubprojects_GGJ2023_RootningAway_Source_RootningAway_RootningAwayGameMode_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Githubprojects_GGJ2023_RootningAway_Source_RootningAway_RootningAwayGameMode_h_12_SPARSE_DATA \
+	FID_Githubprojects_GGJ2023_RootningAway_Source_RootningAway_RootningAwayGameMode_h_12_RPC_WRAPPERS \
+	FID_Githubprojects_GGJ2023_RootningAway_Source_RootningAway_RootningAwayGameMode_h_12_ACCESSORS \
+	FID_Githubprojects_GGJ2023_RootningAway_Source_RootningAway_RootningAwayGameMode_h_12_INCLASS \
+	FID_Githubprojects_GGJ2023_RootningAway_Source_RootningAway_RootningAwayGameMode_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_Githubprojects_GGJ2023_RootningAway_Source_RootningAway_RootningAwayGameMode_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Githubprojects_GGJ2023_RootningAway_Source_RootningAway_RootningAwayGameMode_h_12_SPARSE_DATA \
+	FID_Githubprojects_GGJ2023_RootningAway_Source_RootningAway_RootningAwayGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Githubprojects_GGJ2023_RootningAway_Source_RootningAway_RootningAwayGameMode_h_12_ACCESSORS \
+	FID_Githubprojects_GGJ2023_RootningAway_Source_RootningAway_RootningAwayGameMode_h_12_INCLASS_NO_PURE_DECLS \
+	FID_Githubprojects_GGJ2023_RootningAway_Source_RootningAway_RootningAwayGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ROOTNINGAWAY_API UClass* StaticClass<class ARootningAwayGameMode>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Githubprojects_GGJ2023_RootningAway_Source_RootningAway_RootningAwayGameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
